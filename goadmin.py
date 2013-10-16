057d0923135892f3afa61ccbab224bd64428ad61
Richard Dodd <rdodd@systatic.co.uk>
2013-10-16 11:34:04 +0100

    First proper commit

---
+++ goadmin.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+"""
+Usage: goadmin.py [-h] modulename
+
+Creates a skeleton groupoffice module
+
+"""
+
+from argparse import ArgumentParser
+from jinja2 import Environment, PackageLoader
+import os
+import pwd
+
+if __name__ == "__main__":
+
+	env = Environment(loader=PackageLoader('goadmin', 'templates'))
+	parser = ArgumentParser(description="Administrative functions for " + 
+	        "groupoffice (currently limited to creating new modules)")
+	parser.add_argument('modulename')
+	name = parser.parse_args().modulename
+	
+	# Get username on unix
+	user = (pwd.getpwuid(os.getuid())[4]).split(',')
+	username = user[0]
+	
+	# Create directory structure
+	os.mkdir(name)
+	os.chdir(name)
+	
+	os.mkdir('install')
+	os.mkdir('controller')
+	os.mkdir('language')
+	os.mkdir('model')
+	os.mkdir('views')
+	os.mkdir(os.path.join('views', 'Extjs3'))
+	
+	module_template = env.get_template('NameModule.php')
+	module_file_contents = module_template.render(name=name, username=username)
+	with open(name.capitalize() + 'Module.php', 'w') as f:
+		f.write(module_file_contents)
+	
+	main_panel_template = env.get_template('MainPanel.js')
+	main_panel_file_contents = main_panel_template.render(name=name)
+	with open(os.path.join('views', 'Extjs3', 'MainPanel.js'), 'w') as f:
+		f.write(main_panel_file_contents)
+    
---
+++ templates/Stores.js
@@ -0,0 +1,5 @@
+/*
+ * Use this file to create your stores. See modules/notes/views/Extjs3/Stores.js
+ * for an example.
+ */
+
