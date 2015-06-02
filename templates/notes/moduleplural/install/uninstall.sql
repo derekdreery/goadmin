c0f97e2712494ad6daf5f872c7d4b6b0ec33f94b
Richard Dodd <richard.dodd@itp-group.co.uk>
2015-06-02 12:06:11 +0100

    Module and model done - controller todo

--- templates/notes/moduleplural/install/uninstall.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `cf_{{moduleplural}}_{{moduleplural}}`;
-DROP TABLE IF EXISTS `go_links_{{moduleplural}}_{{moduleplural}}`;
-DROP TABLE IF EXISTS `{{moduleplural}}_categories`;
-DROP TABLE IF EXISTS `{{moduleplural}}_{{moduleplural}}`;
--- templates/simple/moduleplural/language/en.php
+++
@@ -1,3 +0,0 @@
-<?php
-$l['{{modulename}}'] = "{{Modulename}}";
-$l['{{moduleplural}}'] = "{{Moduleplural}}";
